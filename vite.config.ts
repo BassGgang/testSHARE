1b8d3c4a3914df00b5c7f291f485d14391ea07d2
GitHubにBassGgang <jyonketu@gmail.com>
2026-06-06 00:14:25 +0900

    test one uplode

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+      base: '/testSHARE/', // ★ここを追加（前後のスラッシュを忘れずに！）
       server: {
         port: 3000,
         host: '0.0.0.0',
